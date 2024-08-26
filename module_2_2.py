40d42e0bcb8b8a5fcad4ff2f2c6b97ea0dc0da46
Alexander-Logvin <shura.logvin@yandex.ru>
2024-08-26 17:14:18 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,9 @@
 first = input('Введите 1 число: ')
 second = input('Введите 2 число: ')
 third  = input('Введите 3 число: ')
+int_first = int(float(first))
+int_second = int(float(second))
+int_tgird = int(float(third))
 if first == second and second == third and first == third:
     print(3)
 elif first == second or second == third or first == third:
